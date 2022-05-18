a8ccfa5d10e594dbc2933b117d68a91adf1c3146
Barashov <BarashovMisha@gmail.com>
2022-05-18 20:48:51 +0300

    create model affairs

--- TODo/app/models.py
+++ TODo/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Affairs(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+    class Meta:
+        verbose_name = "Affair"
+        verbose_name_plural = "Affairs"
+
+    def __str__(self):
+        return self.name
+
+
