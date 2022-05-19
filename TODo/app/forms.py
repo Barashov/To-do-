5ecedc3962b63d68ebadb6328638d9c32da14162
Barashov <BarashovMisha@gmail.com>
2022-05-19 21:54:16 +0300

    customization add affairs page

--- TODo/app/forms.py
+++ TODo/app/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class AddForm(forms.Form):
-    name = forms.CharField(max_length=50)
-    description = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(max_length=50, label='Название:')
+    description = forms.CharField(widget=forms.Textarea, label='Описание:')
     
